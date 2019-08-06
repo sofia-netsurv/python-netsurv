41729ff5eb31615e1d7774d5a54177b40118dec3
Eliot Woodrich <ekw@babbage.cs.pdx.edu>
2019-08-06 11:10:05 -0700

    Changed directory structure for Pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-netsurv",
+    version="0.1.0",
+    author="Eliot Kent Woodrich",
+    author_email="eliot@woodrich.com",
+    description="Configure IP cameras.",
+    long_description="Python library for configuring a wide range of IP cameras which use the NETsurveillance ActiveX plugin (aka DVRIP, Sofia)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/sofia-client/python-netsurv",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
